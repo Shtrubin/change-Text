ab6c0e4795caffd57bf7e0c2cded49dd6a316614
Rubin <rubeensht17@gmail.com>
2023-08-17 17:48:13 -0700

    Added full functional and properly working text converter and also dark and light mode for whole body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './App.css';
-import About from './component/About';
+// import About from './component/About';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
 function App() {
+
+  const[mode,setMode]= useState('light');
+  const toogleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey';
+    }else
+    {
+      setMode('light')
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-<Navbar title="My App" aboutText="About us"/>
+<Navbar title="My App" mode={mode} toogleMode={toogleMode}/>
 <div className="container">
-<TextForm heading="Enter the text here"/>
-<About/>
+<TextForm heading="Enter the text here" mode={mode}/>
+{/* <About/> */}
 </div>
     </>
   );
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -20,30 +20,41 @@ export default function TextForm(props) {
         let newText= text.split('').reverse().join('');
         SetText(newText);
     }
+    const handleCopyClick=()=>{
+    var text= document.getElementById("exampleFormControlTextarea1");
+       text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleSpaceClick=()=>{
+        let newtext= text.split(/[ ]+/);
+        SetText(newtext.join(" "))
+    }
     const handelonChange=(event)=>{
         SetText(event.target.value);
     }
     const[text,SetText]= useState('Enter the  text now');
   return (
     <>
-    <div className="Container">  
+    <div style= {{color: props.mode==='dark'?'white': 'black'}} className="Container">  
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handelonChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handelonChange} style= {{backgroundColor: props.mode==='dark'?'grey': 'white', color: props.mode==='dark'?'white': 'black'}} id="exampleFormControlTextarea1" rows="8"></textarea>
         </div>
         <button type="button" onClick={handleUpClick} className="btn btn-primary mx-2">UpperCase</button>
         <button type="button" onClick={handleLoClick} className="btn btn-primary mx-2">LowerCase</button>
         <button type="button" onClick={handleClearClick} className="btn btn-primary mx-2">Clear</button>
         <button type="button" onClick={handleRevClick} className="btn btn-primary mx-2">Reverse</button>
+        <button type="button" onClick={handleCopyClick} className="btn btn-primary mx-2">Copy text</button>
+        <button type="button" onClick={handleSpaceClick} className="btn btn-primary mx-2">Remove Extra Space</button>
 
     </div>
-    <div className="container">
+    <div style= {{color: props.mode==='dark'?'white': 'black'}} className="container">
         <h2>Text Summary</h2>
         <p>The text have {text.split(" ").length} word and {text.length} characters</p>
         <p>Time it takes to read {0.002*text.split(" ").length}</p>
     </div>
-    <h3>TEXT PREVIEW</h3>
-    <p>{text}</p>
+    <h3 style= {{color: props.mode==='dark'?'white': 'black'}}>TEXT PREVIEW</h3>
+    <p style= {{color: props.mode==='dark'?'white': 'black'}}>{text.length>0? text:'Enter your text in above textbox to preview'}</p>
     </>
   )
 }
